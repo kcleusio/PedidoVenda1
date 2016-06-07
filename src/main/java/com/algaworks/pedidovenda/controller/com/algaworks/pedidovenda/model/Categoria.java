071f255903baa420bf54bf6e95373324d3db45f6
kcleusioim <kcleusio@gmail.com>
2016-06-07 22:52:07 +0100

    Mapeamento de cluente e endereço

--- src/main/java/com/algaworks/pedidovenda/controller/com/algaworks/pedidovenda/model/Categoria.java
+++ src/main/java/com/algaworks/pedidovenda/controller/com/algaworks/pedidovenda/model/Categoria.java
@@ -1,18 +1,24 @@
 package com.algaworks.pedidovenda.controller.com.algaworks.pedidovenda.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name="categoria")
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
 
 	private Long id;
 	private String descricao;
 	private Categoria categoriaPai;
 	private List<Categoria> subcategorias = new ArrayList<>();
 
+	@Id
+	@GeneratedValue
 	public Long getId() {
 		return id;
 	}
@@ -21,6 +27,7 @@ public class Categoria implements Serializable {
 		this.id = id;
 	}
 
+	@Column(nullable = false, length = 60)
 	public String getDescricao() {
 		return descricao;
 	}
@@ -29,14 +36,18 @@ public class Categoria implements Serializable {
 		this.descricao = descricao;
 	}
 
+	@ManyToOne
+	@JoinColumn(name="categoria_pai_id")
 	public Categoria getCategoriaPai() {
 		return categoriaPai;
 	}
+
 
 	public void setCategoriaPai(Categoria categoriaPai) {
 		this.categoriaPai = categoriaPai;
 	}
 
+	@OneToMany(mappedBy = "categoriaPai", cascade = CascadeType.ALL)
 	public List<Categoria> getSubcategorias() {
 		return subcategorias;
 	}
